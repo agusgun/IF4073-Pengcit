09fa0200d02f352a5fb1fe7a5f8337cde41fc055
agusgun <agoundragon@gmail.com>
2018-09-06 10:10:45 +0700

    Add Histogram Specification (Matching)

--- PengolahanCitra/app/src/main/java/com/pengcit/jorfelag/pengolahancitra/histogram/specification/HistogramSpecificationEqualizedTask.java
+++ PengolahanCitra/app/src/main/java/com/pengcit/jorfelag/pengolahancitra/histogram/specification/HistogramSpecificationEqualizedTask.java
@@ -11,12 +11,10 @@ import android.widget.ImageView;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
-import com.pengcit.jorfelag.pengolahancitra.R;
-import com.pengcit.jorfelag.pengolahancitra.histogram.HistogramActivity;
 
 import java.util.HashMap;
 
-public class HistogramSpecificationEqualizedTask extends AsyncTask<Bitmap, Void, Integer[]> {
+public class HistogramSpecificationEqualizedTask extends AsyncTask<Bitmap, Void, HashMap<String, Integer[]>> {
     private ProgressDialog dialog;
     HistogramSpecificationActivity activity;
     ImageView resultImageView;
@@ -150,46 +148,140 @@ public class HistogramSpecificationEqualizedTask extends AsyncTask<Bitmap, Void,
         return result;
     }
     /**
-     * TBD (src: http://terminalcoders.blogspot.com/2017/02/histogram-equalisation-in-java.html)
-     * @param args The value of referenced histogram.
+     * TBD (src: https://www.youtube.com/watch?v=YxZUnJ_Ok2w)
+     * @param args Original bitmap.
      * @return
      */
     @Override
-    protected Integer[] doInBackground(Bitmap... args) {
+    protected HashMap<String, Integer[]> doInBackground(Bitmap... args) {
         imageBitmap = args[0];
 
-        Integer[] interpolation = new Integer[256];
+        Integer[] templateCumulative = new Integer[256];
+        Integer[] templateForView = new Integer[256];
 
-        //Create Control Point using Normalization
-        interpolation[0] = seekBarValue1;
-        interpolation[127] = seekBarValue2;
-        interpolation[255] = seekBarValue3;
+        templateCumulative[0] = seekBarValue1;
+        templateCumulative[127] = seekBarValue2;
+        templateCumulative[255] = seekBarValue3;
 
         //Todo: change interpolation method
         for (int i = 0; i < 256; i++) {
             if (i == 0 || i == 127 || i == 255) {
                 // do nothing
             } else if (i > 0 && i < 127) {
-                interpolation[i] = ((interpolation[0] * (127 - i) + interpolation[127] * (i - 0)) / (127 - 0));
+                templateCumulative[i] = ((templateCumulative[0] * (127 - i) + templateCumulative[127] * (i - 0)) / (127 - 0));
             } else { // (> 128) (< 255)
-                interpolation[i] = ((interpolation[127] * (255 - i) + interpolation[255] * (i - 127)) / (255 - 127));
+                templateCumulative[i] = ((templateCumulative[127] * (255 - i) + templateCumulative[255] * (i - 127)) / (255 - 127));
             }
-            Log.d("BUGGG" + String.valueOf(i) + " "," " + interpolation[i]);
+            templateForView[i] = templateCumulative[i];
+            Log.d("BUGGG" + String.valueOf(i) + " "," " + templateCumulative[i]);
         }
 
-        return interpolation;
+        Integer[] redValuesFrequencies = new Integer[256];
+        Integer[] greenValuesFrequencies = new Integer[256];
+        Integer[] blueValuesFrequencies = new Integer[256];
+
+        for (int i = 0; i < 256; i++) {
+            redValuesFrequencies[i] = 0;
+            greenValuesFrequencies[i] = 0;
+            blueValuesFrequencies[i] = 0;
+        }
+
+        Bitmap processedBitmap = imageBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        for (int x = 0; x < processedBitmap.getWidth(); x++) {
+            for (int y = 0; y < processedBitmap.getHeight(); y++) {
+                int pixelColor = processedBitmap.getPixel(x, y);
+
+                int red = (pixelColor & 0x00FF0000) >> 16;
+                int green = (pixelColor & 0x0000FF00) >> 8;
+                int blue = (pixelColor & 0x000000FF);
+
+                redValuesFrequencies[red]++;
+                greenValuesFrequencies[green]++;
+                blueValuesFrequencies[blue]++;
+            }
+        }
+
+
+        //Cumulative Distribution
+        for (int i = 1; i < 256; i++) {
+            templateCumulative[i] += templateCumulative[i - 1];
+            redValuesFrequencies[i] += redValuesFrequencies[i - 1];
+            greenValuesFrequencies[i] += greenValuesFrequencies[i - 1];
+            blueValuesFrequencies[i] += blueValuesFrequencies[i - 1];
+        }
+
+        //Normalize
+        int size = processedBitmap.getHeight() * processedBitmap.getWidth();
+
+        for (int i = 0; i < 256; i++) {
+            redValuesFrequencies[i] = (255 * redValuesFrequencies[i]) / size;
+            greenValuesFrequencies[i] = (255 * greenValuesFrequencies[i]) / size;
+            blueValuesFrequencies[i] = (255 * blueValuesFrequencies[i]) / size;
+            templateCumulative[i] = (255 * templateCumulative[i]) / templateCumulative[255];
+            Log.d("BEHE " + i, " " + templateCumulative[i] + " " + redValuesFrequencies[i] + " " + greenValuesFrequencies[i] + " " + blueValuesFrequencies[i]);
+        }
+
+        Integer[] Tred = new Integer[256];
+        Integer[] Tgreen = new Integer[256];
+        Integer[] Tblue = new Integer[256];
+
+        //histogram specification
+        for (int i = 0; i < 256; i++) {
+            int j;
+
+            //Tred
+            j = 255;
+            while (true) {
+                Tred[i] = j;
+                j = j - 1;
+                if (j < 0 || redValuesFrequencies[i] > templateCumulative[j]) {
+                    break;
+                }
+            }
+
+            //Tgreen
+            j = 255;
+            while (true) {
+                Tgreen[i] = j;
+                j = j - 1;
+                if (j < 0 || greenValuesFrequencies[i] > templateCumulative[j]) {
+                    break;
+                }
+            }
+
+            //Tblue
+            j = 255;
+            while (true) {
+                Tblue[i] = j;
+                j = j - 1;
+                if (j < 0 || blueValuesFrequencies[i] > templateCumulative[j]) {
+                    break;
+                }
+            }
+
+        }
+
+        HashMap<String, Integer[]> result = new HashMap<>();
+        result.put("templateForView", templateForView);
+        result.put("Tred", Tred);
+        result.put("Tgreen", Tgreen);
+        result.put("Tblue", Tblue);
+
+        return result;
     }
 
     /**
      * Display image histogram.
      * @param result referenced histogram.
      */
-    protected void onPostExecute(Integer[] result) {
+    protected void onPostExecute(HashMap<String, Integer[]> result) {
         if (dialog.isShowing()) {
             dialog.dismiss();
         }
 
-        BarGraphSeries series = new BarGraphSeries<>(generateData(result));
+        Integer[] template = result.get("templateForView");
+
+        BarGraphSeries series = new BarGraphSeries<>(generateData(template));
         series.setColor(Color.BLACK);
 
         referencedHistogramView.addSeries(series);
@@ -202,8 +294,26 @@ public class HistogramSpecificationEqualizedTask extends AsyncTask<Bitmap, Void,
         referencedHistogramView.getViewport().setScalable(true);
         referencedHistogramView.getViewport().setScalableY(true);
 
-        //Todo: histogram specification here
-        resultImageView.setImageBitmap(imageBitmap);
+
+        Integer[] Tred = result.get("Tred");
+        Integer[] Tgreen = result.get("Tgreen");
+        Integer[] Tblue = result.get("Tblue");
+
+        Bitmap processedBitmap = imageBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        Bitmap resultImage = Bitmap.createBitmap(imageBitmap.getWidth(), imageBitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        for (int x = 0; x < processedBitmap.getWidth(); x++) {
+            for (int y = 0; y < processedBitmap.getHeight(); y++) {
+                int pixelColor = processedBitmap.getPixel(x, y);
+
+                int red = (pixelColor & 0x00FF0000) >> 16;
+                int green = (pixelColor & 0x0000FF00) >> 8;
+                int blue = (pixelColor & 0x000000FF);
+
+                int newPixelColor = (0xFF<<24) | (Tred[red]<<16) | (Tgreen[green]<<8) | Tblue[blue];
+                resultImage.setPixel(x, y, newPixelColor);
+            }
+        }
+        resultImageView.setImageBitmap(resultImage);
     }
 
     private DataPoint[] generateData(Integer[] colorValuesFrequencies) {
