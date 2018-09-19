293c98947f5e48f36cccb5678122d991bc5f6e8f
agusgun <agoundragon@gmail.com>
2018-09-19 21:49:24 +0700

    Add Uploader

--- WebPengolahanCitra/app.py
+++ WebPengolahanCitra/app.py
@@ -1,42 +1,54 @@
+import os
 from flask import Flask, request, redirect, url_for
 
+UPLOAD_FOLDER = '/tmp/'
+ALLOWED_EXTENSIONS = set(['jpg', 'png', 'gif'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
   return ("Yo, it's working!")
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
 @app.route('/predict_digit', methods=['GET', 'POST'])
 def predict_digit():
-  if request.methods='POST':
+  if request.method == 'POST':
     print('YAHAI')
-  else:
-    return """
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    <p>%s</p>
+  return """
+  <!doctype html>
+  <title>Upload new File</title>
+  <h1>Upload new File</h1>
+  <form action="" method=post enctype=multipart/form-data>
+    <p><input type=file name=file>
+       <input type=submit value=Upload>
+  </form>
+  <p>%s</p>
     """ % "<br>".join(os.listdir(app.config['UPLOAD_FOLDER'],))
 
 @app.route('/image_thinning', methods=['GET', 'POST'])
 def image_thinning():
-  if request.methods='POST':
-    print('YAHAI')
-  else:
-    return """
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    <p>%s</p>
-    """ % "<br>".join(os.listdir(app.config['UPLOAD_FOLDER'],))  
+  if request.method == 'POST':
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('index'))
+  return """
+  <!doctype html>
+  <title>Upload new File</title>
+  <h1>Upload new File</h1>
+  <form action="" method=post enctype=multipart/form-data>
+    <p><input type=file name=file>
+       <input type=submit value=Upload>
+  </form>
+  <p>%s</p>
+  """ % "<br>".join(os.listdir(app.config['UPLOAD_FOLDER'],))  
 
 if __name__ == "__main__":
   app.run()
