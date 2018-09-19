e3ec2b55c477ddd0e214ea9a20f7888af76d861d
agusgun <agoundragon@gmail.com>
2018-09-19 21:25:59 +0700

    Move Requirements and Add New Route

--- WebPengolahanCitra/app.py
+++ WebPengolahanCitra/app.py
@@ -1,10 +1,42 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return ("Yo, it's working!")
+
+@app.route('/predict_digit', methods=['GET', 'POST'])
+def predict_digit():
+  if request.methods='POST':
+    print('YAHAI')
+  else:
+    return """
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    <p>%s</p>
+    """ % "<br>".join(os.listdir(app.config['UPLOAD_FOLDER'],))
+
+@app.route('/image_thinning', methods=['GET', 'POST'])
+def image_thinning():
+  if request.methods='POST':
+    print('YAHAI')
+  else:
+    return """
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    <p>%s</p>
+    """ % "<br>".join(os.listdir(app.config['UPLOAD_FOLDER'],))  
 
 if __name__ == "__main__":
   app.run()
